83e465f8944d0ddf5cbea39e326239a9cede3a94
RohanShaikhh <soc.solutions.rohaan@outlook.com>
2023-04-13 15:28:01 +0500

    updated

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ app.use(
     extended: true,
   })
 );
-app.use(cors())
-// const whitelist = ["https://freeswap.co.in"];
+// app.use(cors())
+const whitelist = ["http://104.248.130.142:3000"];
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 //////////////////////////////////////
 // app.post("/transfertokenzax", async (req, res) => {
 //   var receiptAddress = req.body.walletAddress
