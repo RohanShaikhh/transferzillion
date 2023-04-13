d12ce0c8ee3fae23e4a20e7d5fc7d21664e25205
RohanShaikhh <soc.solutions.rohaan@outlook.com>
2023-04-13 17:22:23 +0500

    updated

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const http = require("http");
 const axios = require('axios')
 require('dotenv').config();
 const server = http.createServer(app);
-const {ethers,JsonRpcProvider , formatEther, parseUnits, isAddress, ContractTransactionResponse, InfuraProvider} = require("ethers");
+const { ethers, JsonRpcProvider, formatEther, parseUnits, isAddress, ContractTransactionResponse, InfuraProvider } = require("ethers");
 
-const HttpProvider =
-  "https://eth-mainnet.g.alchemy.com/v2/3iz35aSwwC5nbTT9SyTmJ0WM916nuv70";
+const HttpProvider = "https://eth-mainnet.g.alchemy.com/v2/3iz35aSwwC5nbTT9SyTmJ0WM916nuv70";
 app.use(bodyParser.json({ limit: "100mb", type: "application/json" }));
 app.use(
   bodyParser.urlencoded({
@@ -18,7 +17,6 @@ app.use(
     extended: true,
   })
 );
-// app.use(cors())
 const whitelist = ["http://104.248.130.142:3000"];
 
 const corsOptions = {
@@ -32,96 +30,62 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-//////////////////////////////////////
-// app.post("/transfertokenzax", async (req, res) => {
-//   var receiptAddress = req.body.walletAddress
-//   await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=Zillion-Aakar-XO&vs_currencies=usd')
-//   .then(async function (response) {
-//     var amount = req.body.token
-//   console.log("amount "+amount)
-//  var zillionAmount = amount/response.data['zillion-aakar-xo'].usd
-//  console.log("amount zillion "+zillionAmount.toFixed(3))
-//  var valueInString = zillionAmount.toFixed(3).toString()
-//  console.log("valueInString "+valueInString)
-//   console.log("wallet address "+receiptAddress)
-  
-//   var CONTRACT_ADDRESS = "0x9A2478C4036548864d96a97Fbf93f6a3341fedac"
-//   var privateKey = process.env.PRIVATE_KEY
-//   privateKey = "0x".concat(privateKey)
-//   const abi = require("./zaxabi.json")
 
-// const provider = new JsonRpcProvider("https://bsc-dataseed.binance.org/"); // Connect to Ropsten testnet
-// const wallet = new ethers.Wallet(privateKey, provider);
-// const amountConvert = parseUnits(valueInString,9)
-// const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, wallet);
-// if(isAddress(receiptAddress)){
-//   try{
-// const tx = await contract.transfer(receiptAddress, amountConvert);
-// console.log('Transaction hash:', tx.hash);
-// const result = {
-//   response : tx.hash
-// }
-//  return res.status(200).send(result)
-// }
-// catch(err){
-//   console.log("Insufficient Funds")
-//   return res.status(401).send("Insufficient Balance")
-// }
-// }
-// else{
-//   console.log("Invalid Address")
-//   const result = {
-//     response : "Invalid Address"
-//   }
-//   return res.status(400).send(result)
-// }
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
-// });
+const requestQueue = [];
+let isProcessingQueue = false;
+
 app.post("/transfertokenzax", async (req, res) => {
-  try {
-    const receiptAddress = req.body.walletAddress;
-    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=Zillion-Aakar-XO&vs_currencies=usd');
-    const amount = req.body.token;
-    console.log("amount " + amount);
-    const zillionAmount = amount / response.data['zillion-aakar-xo'].usd;
-    console.log("amount zillion " + zillionAmount.toFixed(3));
-    const valueInString = zillionAmount.toFixed(3).toString();
-    console.log("valueInString " + valueInString);
-    console.log("wallet address " + receiptAddress);
+  requestQueue.push({ req, res });
 
-    const CONTRACT_ADDRESS = "0x9A2478C4036548864d96a97Fbf93f6a3341fedac";
-    let privateKey = process.env.PRIVATE_KEY;
-    privateKey = "0x" + privateKey;
-    const abi = require("./zaxabi.json");
+  if (!isProcessingQueue) {
+    isProcessingQueue = true;
 
-    const provider = new JsonRpcProvider("https://bsc-dataseed.binance.org/");
-    const wallet = new ethers.Wallet(privateKey, provider);
-    const amountConvert = parseUnits(valueInString, 9);
-    const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, wallet);
+    while (requestQueue.length > 0) {
+      const { req, res } = requestQueue.shift();
 
-    if (isAddress(receiptAddress)) {
-      const tx = await contract.transfer(receiptAddress, amountConvert);
-      console.log('Transaction hash:', tx.hash);
-      const result = {
-        response: tx.hash
-      };
-      return res.status(200).send(result);
-    } else {
-      console.log("Invalid Address");
-      const result = {
-        response: "Invalid Address"
-      };
-      return res.status(400).send(result);
+      try {
+        const receiptAddress = req.body.walletAddress;
+        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=Zillion-Aakar-XO&vs_currencies=usd');
+        const amount = req.body.token;
+        console.log("amount " + amount);
+        const zillionAmount = amount / response.data['zillion-aakar-xo'].usd;
+        console.log("amount zillion " + zillionAmount.toFixed(3));
+        const valueInString = zillionAmount.toFixed(3).toString();
+        console.log("valueInString " + valueInString);
+        console.log("wallet address " + receiptAddress);
+
+        const CONTRACT_ADDRESS = "0x9A2478C4036548864d96a97Fbf93f6a3341fedac";
+        let privateKey = process.env.PRIVATE_KEY;
+        privateKey = "0x" + privateKey;
+        const abi = require("./zaxabi.json");
+
+        const provider = new JsonRpcProvider("https://bsc-dataseed.binance.org/");
+        const wallet = new ethers.Wallet(privateKey, provider);
+        const amountConvert = parseUnits(valueInString, 9);
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, wallet);
+
+        if (isAddress(receiptAddress)) {
+          const tx = await contract.transfer(receiptAddress, amountConvert);
+          console.log('Transaction hash:', tx.hash);
+          const result = {
+            response: tx.hash
+          };
+          res.status(200).send(result);
+        } else {
+          console.log("Invalid Address");
+          const result = {
+            response: "Invalid Address"
+          };
+          res.status(400).send(result);
+        }
+      } catch (err) {
+        console.log("Insufficient Funds");
+        res.status(401).send("Insufficient Balance");
+      }
     }
-  } catch (err) {
-    console.log("Insufficient Funds");
-    return res.status(401).send("Insufficient Balance");
+
+    isProcessingQueue = false;
   }
 });
-
-//////////////////////////////////////
 
 server.listen(PORT, () => console.log(`running on port ${PORT}`));
